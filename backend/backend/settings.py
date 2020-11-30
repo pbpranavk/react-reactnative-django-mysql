82a2935ba237700c2afd71d3f0549c3282af5263
Pranav Kumar <pbpranav24@gmail.com>
2020-11-30 12:58:42 +0530

    Test custom query in react-query

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "corsheaders",
     "rest_framework",
     "simple_crud.apps.SimpleCrudConfig",
     "django.contrib.admin",
@@ -42,6 +43,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -49,6 +52,17 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+CORS_ORIGIN_ALLOW_ALL = (
+    True  # If this is used then `CORS_ORIGIN_WHITELIST` will not have any effect
+)
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:3001",
+]  # If this is used, then not need to use `CORS_ORIGIN_ALLOW_ALL = True`
+CORS_ORIGIN_REGEX_WHITELIST = [
+    "http://localhost:3001",
 ]
 
 ROOT_URLCONF = "backend.urls"
--- web/src/App.js
+++ web/src/App.js
@@ -1,23 +1,119 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import axios from "axios";
+import { useQuery, useMutation } from "react-query";
+
+const getItemsApi = async () => {
+  const { data } = await axios.get("http://localhost:8090/simple_route/");
+  console.log({ data });
+  return data;
+};
+
+const postItemApi = async ({ text }) => {
+  const { data } = await axios.post("http://localhost:8090/simple_route/", {
+    text,
+  });
+  return data;
+};
+
+const useGetItems = (shouldFetch = true, setShouldFetch = () => {}) => {
+  const returnParams = useQuery(["getItems", shouldFetch], getItemsApi, {
+    onSuccess: (data) => {
+      setShouldFetch(false);
+      return data;
+    },
+    onError: (error) => {
+      console.error(error);
+      setShouldFetch(false);
+    },
+  });
+  return returnParams;
+};
+
+const ShowDataItems = () => {
+  const { data, status, isLoading, isFetching } = useGetItems();
+
+  if (status === "loading" || isLoading || isFetching) {
+    return "Loading";
+  }
+
+  console.log({ data });
+  return (
+    <>
+      {data?.map((item) => (
+        <p key={item.text}>{item.text}</p>
+      ))}
+    </>
+  );
+};
+
+const ShowAndPushDataItems = () => {
+  const [newItem, setNewItem] = useState("");
+  const [shouldFetchData, setShouldFetchData] = useState(true);
+
+  const { data, status, isLoading, isFetching } = useGetItems(
+    shouldFetchData,
+    setShouldFetchData
+  );
+  const [
+    postItem,
+    {
+      status: postStatus,
+      isLoading: postIsLoading,
+      isFetching: postISFetching,
+    },
+  ] = useMutation(postItemApi, {
+    onSuccess: () => {
+      setShouldFetchData(true);
+    },
+    onError: (error) => {
+      console.log({ error });
+    },
+  });
+
+  if (
+    status === "loading" ||
+    isLoading ||
+    isFetching ||
+    postStatus === "loading" ||
+    postIsLoading ||
+    postISFetching
+  ) {
+    return "Loading";
+  }
+  return (
+    <>
+      {data?.map((item) => (
+        <p key={item.text}>{item.text}</p>
+      ))}
+      <input
+        value={newItem}
+        onChange={(e) => {
+          setNewItem(e.target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          postItem({ text: newItem });
+        }}
+      >
+        Push item
+      </button>
+    </>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ display: "flex", width: "100%" }}>
+      <div style={{ width: "50%", borderRight: "1px solid grey" }}>
+        <p>Just data</p>
+        <ShowDataItems />
+      </div>
+      <div style={{ width: "45%" }}>
+        <p>Data and Push</p>
+        <ShowAndPushDataItems />
+      </div>
     </div>
   );
 }

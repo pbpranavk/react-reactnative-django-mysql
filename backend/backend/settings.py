00683399664fbb2ffe8155245142b76fb6180c91
Pranav Kumar <pbpranav24@gmail.com>
2020-12-05 10:03:39 +0530

    Migrate from sqlite to mysql

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -91,8 +91,12 @@ WSGI_APPLICATION = "backend.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "mysqldjango",
+        "USER": "root",
+        "PASSWORD": "",
+        "HOST": "localhost",
+        "PORT": "3306",
     }
 }
 
